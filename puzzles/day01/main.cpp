6640c2fa0d019def47cbf8df99da7eb74a3a8656
Bryan Covell <bpc6@vt.edu>
2024-12-02 14:34:54 -0700

    read in data

--- puzzles/day01/main.cpp
+++ puzzles/day01/main.cpp
@@ -1,6 +1,28 @@
+#include "parsing.h"
 #include <iostream>
+#include <ranges>
+#include <vector>
+
+std::vector<int> get_col(const std::string &filepath, int colnum) {
+  auto lines = readLines(filepath);
+  std::vector<int> result;
+  result.reserve(lines.size());
+  for (const auto &line : lines) {
+    result.push_back(ints(line)[colnum]);
+  }
+  return result;
+}
 
 int main() {
-  std::cout << "hello" << std::endl;
+  auto col0 = get_col(PARENT_DIR "/test.txt", 0);
+  auto col1 = get_col(PARENT_DIR "/test.txt", 1);
+
+  std::sort(col0.begin(), col0.end());
+  std::sort(col1.begin(), col1.end());
+
+  for (const auto &val : col0) {
+    std::cout << val << " ";
+  }
+  std::cout << std::endl;
   return EXIT_SUCCESS;
 }
