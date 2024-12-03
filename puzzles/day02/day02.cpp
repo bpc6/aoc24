8f9ba39695b4064d0133d58dba700fbdb57c9d7d
Bryan Covell <bpc6@vt.edu>
2024-12-03 11:49:58 -0700

    day2 template

---
+++ puzzles/day02/day02.cpp
@@ -0,0 +1,9 @@
+#include "day02.h"
+#include "parsing.h"
+#include <algorithm>
+#include <numeric>
+#include <ranges>
+
+int part1(const std::string &filename) { return 0; }
+
+int part2(const std::string &filename) { return 0; }
