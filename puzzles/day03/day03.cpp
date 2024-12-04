901c025e0bbf5222ec4feca353e9d9f9c4df9764
Bryan Covell <bpc6@vt.edu>
2024-12-03 19:23:37 -0700

    part 1

--- puzzles/day03/day03.cpp
+++ puzzles/day03/day03.cpp
@@ -1,9 +1,20 @@
 #include "day03.h"
 #include "parsing.h"
-#include <algorithm>
-#include <numeric>
-#include <ranges>
+#include <regex>
 
-long part1(const std::string &filename) { return 0; }
+long part1(const std::string &filename) {
+  auto txt = readFile(PARENT_DIR "/" + filename);
+
+  const std::regex re(R"(mul\((-?\d+),(-?\d+)\))");
+  std::smatch match;
+
+  int tot = 0;
+  for (std::sregex_iterator it(txt.begin(), txt.end(), re), end; it != end;
+       ++it) {
+    tot += stoi((*it)[1]) * stoi((*it)[2]);
+  }
+
+  return tot;
+}
 
 long part2(const std::string &filename) { return 0; }
