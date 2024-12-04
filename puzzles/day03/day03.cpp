af3b796312985ade068c74aece9251761d49de5f
Bryan Covell <bpc6@vt.edu>
2024-12-03 19:33:00 -0700

    cleanup & start pt 2

--- puzzles/day03/day03.cpp
+++ puzzles/day03/day03.cpp
@@ -5,11 +5,10 @@
 long part1(const std::string &filename) {
   auto txt = readFile(PARENT_DIR "/" + filename);
 
-  const std::regex re(R"(mul\((-?\d+),(-?\d+)\))");
-  std::smatch match;
+  const std::regex mul(R"(mul\((-?\d+),(-?\d+)\))");
 
   int tot = 0;
-  for (std::sregex_iterator it(txt.begin(), txt.end(), re), end; it != end;
+  for (std::sregex_iterator it(txt.begin(), txt.end(), mul), end; it != end;
        ++it) {
     tot += stoi((*it)[1]) * stoi((*it)[2]);
   }
@@ -17,4 +16,4 @@ long part1(const std::string &filename) {
   return tot;
 }
 
-long part2(const std::string &filename) { return 0; }
+long part2(const std::string &filename) { return part1(filename); }
--- puzzles/day03/day03_test.cpp
+++ puzzles/day03/day03_test.cpp
@@ -2,4 +2,4 @@
 #include <gtest/gtest.h>
 
 TEST(Day03Test, Part1) { EXPECT_EQ(part1("test.txt"), 161); }
-TEST(Day03Test, Part2) { EXPECT_EQ(part2("test.txt"), 0); }
+TEST(Day03Test, Part2) { EXPECT_EQ(part2("test.txt"), 48); }
