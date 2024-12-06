6b7df3a624c9159c0d5a1520479eefe1fa419c14
Bryan Covell <bpc6@vt.edu>
2024-12-05 22:49:42 -0700

    5.1 done

--- puzzles/day05/day05.cpp
+++ puzzles/day05/day05.cpp
@@ -4,24 +4,67 @@
 #include <numeric>
 #include <ranges>
 
-std::vector<std::pair<int, int>> read_rules(const std::vector<std::string> &lines) {
-    auto has_pipe = [](const std::string &s) {return s.find('|') != std::string::npos;};
-    auto make_pair = [](const std::string &s) {return std::make_pair(std::stoi(s.substr(0, 2)), std::stoi(s.substr(3, 5)));};
-    auto pairs = lines | std::views::filter(has_pipe) | std::views::transform(make_pair);
-    return {pairs.begin(), pairs.end()};
+std::map<int, std::vector<int>>
+read_map(const std::vector<std::string> &lines) {
+  auto has_pipe = [](const std::string &s) {
+    return s.find('|') != std::string::npos;
+  };
+  auto make_pair = [](const std::string &s) {
+    return std::make_pair(std::stoi(s.substr(0, 2)), std::stoi(s.substr(3, 5)));
+  };
+
+  std::map<int, std::vector<int>> map;
+  for (const auto [left, right] : lines | std::views::filter(has_pipe) |
+                                      std::views::transform(make_pair)) {
+    if (!map.contains(left)) {
+      map.insert({left, {}});
+    }
+    map[left].push_back(right);
+  }
+  return map;
 }
 
-std::vector<std::vector<int>> read_page_updates(const std::vector<std::string> &lines) {
-    auto has_comma = [](const std::string &s) {return s.find(',') != std::string::npos;};
-    auto to_ints = [](const std::string &s) {return ints(s, ',');};
-    auto int_vecs = lines | std::views::filter(has_comma) | std::views::transform(to_ints);
-    return {int_vecs.begin(), int_vecs.end()};
+std::vector<std::vector<int>>
+read_page_updates(const std::vector<std::string> &lines) {
+  auto has_comma = [](const std::string &s) {
+    return s.find(',') != std::string::npos;
+  };
+  auto to_ints = [](const std::string &s) { return ints(s, ','); };
+  auto int_vecs =
+      lines | std::views::filter(has_comma) | std::views::transform(to_ints);
+  return {int_vecs.begin(), int_vecs.end()};
+}
+
+bool correct_order(const std::vector<int> update,
+                   std::map<int, std::vector<int>> rules) {
+  for (auto [idx, left] : update | std::views::enumerate) {
+    for (auto right : update | std::views::drop(idx + 1)) {
+      if (std::ranges::find(rules[left], right) == rules[left].end()) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+int middle(const std::vector<int> &v) {
+  if (v.size() == 0) {
+    return 0;
+  }
+  return v[static_cast<int>(v.size() / 2)];
 }
 
 long part1(const std::string &filename) {
-  return 0;
+  auto lines = readLines(PARENT_DIR "/" + filename);
+  auto rules = read_map(lines);
+  auto updates = read_page_updates(lines);
+
+  auto correct_middles = updates |
+                         std::views::filter([&rules](std::vector<int> v) {
+                           return correct_order(v, rules);
+                         }) |
+                         std::views::transform(middle);
+  return std::ranges::fold_left(correct_middles, 0, std::plus<int>());
 }
 
-long part2(const std::string &filename) {
-  return 0;
-}
+long part2(const std::string &filename) { return 0; }
--- puzzles/day05/day05.h
+++ puzzles/day05/day05.h
@@ -1,12 +1,15 @@
 #ifndef AOC24_DAY05_H
 #define AOC24_DAY05_H
 
+#include <map>
 #include <string>
 #include <vector>
 
-std::vector<std::pair<int, int>> read_rules(const std::vector<std::string> &lines);
-std::vector<std::vector<int>> read_page_updates(const std::vector<std::string> &lines);
-
+std::map<int, std::vector<int>> read_map(const std::vector<std::string> &lines);
+std::vector<std::vector<int>>
+read_page_updates(const std::vector<std::string> &lines);
+bool correct_order(const std::vector<int> update,
+                   std::map<int, std::vector<int>> rules);
 
 long part1(const std::string &filename);
 long part2(const std::string &filename);
