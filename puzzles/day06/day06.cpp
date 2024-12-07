e9ff02942ac6d02c823496e099692581143a1e29
Bryan Covell <bpc6@vt.edu>
2024-12-07 14:04:42 -0700

    6.1 done

--- puzzles/day06/day06.cpp
+++ puzzles/day06/day06.cpp
@@ -1,9 +1,31 @@
 #include "day06.h"
+#include "grid.h"
 #include "parsing.h"
-#include <algorithm>
-#include <numeric>
-#include <ranges>
 
-long part1(const std::string &filename) { return 0; }
+Eigen::Vector2i rotate(const Eigen::Vector2i &dir) {
+  Eigen::Matrix2i rot{{0, -1}, {1, 0}};
+  return rot * dir;
+}
+
+int count_visited(const Grid<char> &grid) {
+  Grid<int> visited(grid.height(), grid.width(), 0);
+  auto curr = grid.find_coords('^')[0];
+  Eigen::Vector2i direction{0, -1}, next;
+
+  while (grid.has_coord(next = curr + direction)) {
+    visited[curr] = 1;
+    if (grid[next] == '#') {
+      direction = rotate(direction);
+    } else {
+      curr = next;
+    }
+  }
+
+  return 1 + visited.find_coords(1).size();
+}
+
+long part1(const std::string &filename) {
+  return count_visited(Grid<char>(readCharMatrix(PARENT_DIR "/" + filename)));
+}
 
 long part2(const std::string &filename) { return 0; }
--- puzzles/day06/day06_test.cpp
+++ puzzles/day06/day06_test.cpp
@@ -1,5 +1,7 @@
 #include "day06.h"
+#include "grid.h"
 #include <gtest/gtest.h>
 
-TEST(Day01Test, Part1) { EXPECT_EQ(part1("test.txt"), 0); }
-TEST(Day01Test, Part2) { EXPECT_EQ(part2("test.txt"), 0); }
+TEST(CountVisited, Basic) { EXPECT_EQ(part1("test_small.txt"), 6); }
+TEST(Day01Test, Part1) { EXPECT_EQ(part1("test.txt"), 41); }
+TEST(Day01Test, Part2) { EXPECT_EQ(part2("test.txt"), 6); }
