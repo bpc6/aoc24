3acfb804bd2a652eb5766b44a91c74974f0e0650
Bryan Covell <bpc6@vt.edu>
2024-12-09 12:42:58 -0700

    day07 template

---
+++ puzzles/day07/day07.cpp
@@ -0,0 +1,9 @@
+#include "day07.h"
+#include "parsing.h"
+#include <algorithm>
+#include <numeric>
+#include <ranges>
+
+long part1(const std::string &filename) { return 0; }
+
+long part2(const std::string &filename) { return 0; }
---
+++ puzzles/day07/day07_test.cpp
@@ -0,0 +1,5 @@
+#include "day07.h"
+#include <gtest/gtest.h>
+
+TEST(day07Test, Part1) { EXPECT_EQ(part1("test.txt"), 0); }
+TEST(day07Test, Part2) { EXPECT_EQ(part2("test.txt"), 0); }
