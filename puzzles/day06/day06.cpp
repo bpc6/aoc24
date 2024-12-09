17977d01b6d732ba76c1015d5c9b0f2d3096dd0c
Bryan Covell <bpc6@vt.edu>
2024-12-09 09:18:15 -0700

    passed tests but still failing overall

--- puzzles/day06/day06.cpp
+++ puzzles/day06/day06.cpp
@@ -7,7 +7,7 @@ Eigen::Vector2i rotate(const Eigen::Vector2i &dir) {
   return rot * dir;
 }
 
-int count_visited(const Grid<char> &grid) {
+size_t count_visited(const Grid<char> &grid) {
   Grid<int> visited(grid.height(), grid.width(), 0);
   auto curr = grid.find_coords('^')[0];
   Eigen::Vector2i direction{0, -1}, next;
@@ -24,8 +24,46 @@ int count_visited(const Grid<char> &grid) {
   return 1 + visited.find_coords(1).size();
 }
 
+int check_for_loop(const Grid<char> &grid,
+                   const Grid<std::vector<Eigen::Vector2i>> &visited,
+                   Eigen::Vector2i curr, const Eigen::Vector2i &dir) {
+  auto new_dir = rotate(dir);
+  Eigen::Vector2i next;
+  while (grid.has_coord(next = curr + new_dir) && grid[next] != '#') {
+    //    auto found_val = visited[curr].has_value() ? visited[curr].value() :
+    //    Eigen::Vector2i{0,0};
+    if (std::find(visited[curr].begin(), visited[curr].end(), new_dir) !=
+        visited[curr].end()) {
+      return 1;
+    }
+    curr = next;
+  }
+  return 0;
+}
+
+int count_loopers(Grid<char> grid) {
+  Grid<std::vector<Eigen::Vector2i>> visited(grid.height(), grid.width(), {});
+  auto curr = grid.find_coords('^')[0];
+  Eigen::Vector2i direction{0, -1}, next;
+
+  int counter = 0;
+
+  while (grid.has_coord(next = curr + direction)) {
+    if (grid[next] == '#') {
+      direction = rotate(direction);
+    } else {
+      counter += check_for_loop(grid, visited, curr, direction);
+      curr = next;
+    }
+    visited[curr].push_back(direction);
+  }
+  return counter;
+}
+
 long part1(const std::string &filename) {
   return count_visited(Grid<char>(readCharMatrix(PARENT_DIR "/" + filename)));
 }
 
-long part2(const std::string &filename) { return 0; }
+long part2(const std::string &filename) {
+  return count_loopers(Grid<char>(readCharMatrix(PARENT_DIR "/" + filename)));
+}
