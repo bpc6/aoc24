e292e10766f5561e57d0c1d713cfed0656cb1806
Bryan Covell <bpc6@vt.edu>
2024-12-10 21:53:05 -0700

    extract method for finding points in pt2

--- puzzles/day08/day08.cpp
+++ puzzles/day08/day08.cpp
@@ -28,6 +28,20 @@ long part1(const std::string &filename) {
   return found.size();
 }
 
+void add_inline_coords_to_vec(const Grid<char> &grid,
+                              const Eigen::Vector2i &coord,
+                              const Eigen::Vector2i &delta,
+                              std::vector<Eigen::Vector2i> &found) {
+  int scale = 0;
+  Eigen::Vector2i p;
+  while (grid.has_coord(p = coord - scale * delta)) {
+    if (std::ranges::find(found, p) == found.end()) {
+      found.push_back(p);
+    }
+    ++scale;
+  }
+}
+
 long part2(const std::string &filename) {
   Grid grid{readCharMatrix(PARENT_DIR "/" + filename)};
 
@@ -40,23 +54,8 @@ long part2(const std::string &filename) {
     const auto coords = grid.find_coords(c);
     for (const auto [idx, coord] : std::views::enumerate(coords)) {
       for (const auto next : std::views::drop(coords, idx + 1)) {
-        const auto delta = next - coord;
-
-        int scale = 0;
-        Eigen::Vector2i p;
-        while (grid.has_coord(p = coord - scale * delta)) {
-          if (std::ranges::find(found, p) == found.end()) {
-            found.push_back(p);
-          }
-          ++scale;
-        }
-        scale = 0;
-        while (grid.has_coord(p = next + scale * delta)) {
-          if (std::ranges::find(found, p) == found.end()) {
-            found.push_back(p);
-          }
-          ++scale;
-        }
+        add_inline_coords_to_vec(grid, coord, next - coord, found);
+        add_inline_coords_to_vec(grid, coord, coord - next, found);
       }
     }
   }
