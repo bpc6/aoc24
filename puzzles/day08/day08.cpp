48b3a54ef67f278ca6c748cd06258b746f10bbfb
Bryan Covell <bpc6@vt.edu>
2024-12-10 21:40:11 -0700

    finished part 2 but ugly code

--- puzzles/day08/day08.cpp
+++ puzzles/day08/day08.cpp
@@ -28,4 +28,37 @@ long part1(const std::string &filename) {
   return found.size();
 }
 
-long part2(const std::string &filename) { return 0; }
+long part2(const std::string &filename) {
+  Grid grid{readCharMatrix(PARENT_DIR "/" + filename)};
+
+  std::vector<Eigen::Vector2i> found;
+  for (const auto c : grid.find_unique()) {
+    if (c == '.') {
+      continue;
+    }
+
+    const auto coords = grid.find_coords(c);
+    for (const auto [idx, coord] : std::views::enumerate(coords)) {
+      for (const auto next : std::views::drop(coords, idx + 1)) {
+        const auto delta = next - coord;
+
+        int scale = 0;
+        Eigen::Vector2i p;
+        while (grid.has_coord(p = coord - scale * delta)) {
+          if (std::ranges::find(found, p) == found.end()) {
+            found.push_back(p);
+          }
+          ++scale;
+        }
+        scale = 0;
+        while (grid.has_coord(p = next + scale * delta)) {
+          if (std::ranges::find(found, p) == found.end()) {
+            found.push_back(p);
+          }
+          ++scale;
+        }
+      }
+    }
+  }
+  return found.size();
+}
--- puzzles/day08/day08_test.cpp
+++ puzzles/day08/day08_test.cpp
@@ -3,4 +3,4 @@
 
 TEST(Day08Test, Part1Basic) { EXPECT_EQ(part1("test_basic.txt"), 4); }
 TEST(Day08Test, Part1) { EXPECT_EQ(part1("test.txt"), 14); }
-TEST(Day08Test, Part2) { EXPECT_EQ(part2("test.txt"), 0); }
+TEST(Day08Test, Part2) { EXPECT_EQ(part2("test.txt"), 34); }
