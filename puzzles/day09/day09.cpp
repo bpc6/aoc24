4e8e63ad408b328966bc1fc98803ee3a31e07040
Bryan Covell <bpc6@vt.edu>
2024-12-12 17:35:21 -0700

    extract some methods

--- puzzles/day09/day09.cpp
+++ puzzles/day09/day09.cpp
@@ -4,6 +4,28 @@
 #include <numeric>
 #include <ranges>
 
+
+bool back_behind_front(size_t back_fid, size_t front_fid) { return back_fid >= front_fid; }
+
+void move_back_to_first_available(size_t back_fid, std::vector<size_t> &result, int &back_remaining,
+                                  int &front_remaining) {
+  result.push_back(back_fid);
+  front_remaining -= 1;
+  back_remaining -= 1;
+}
+
+void skip_to_next_file(size_t &back_fid,
+                       std::basic_string<char, std::char_traits<char>, std::allocator<char>>::const_reverse_iterator &back_it,
+                       int &back_remaining) {
+  back_it += 2;
+  back_remaining = *back_it - '0';
+  back_fid -= 1;
+}
+
+bool space_is_free(int front_remaining) {
+  return front_remaining > 0;
+}
+
 std::vector<size_t> compress_disc(const std::string &discmap) {
   auto back_fid = discmap.size() / 2;
   bool back_is_file = discmap.size() % 2;
@@ -28,15 +50,11 @@ std::vector<size_t> compress_disc(const std::string &discmap) {
         ++back_it;
         back_is_file = !back_is_file;
       }
-      while (front_remaining > 0 && back_fid >= front_fid) {
+      while (space_is_free(front_remaining) && back_behind_front(back_fid, front_fid)) {
         if (back_remaining == 0) {
-          back_it += 2;
-          back_remaining = *back_it - '0';
-          back_fid -= 1;
+          skip_to_next_file(back_fid, back_it, back_remaining);
         } else {
-          result.push_back(back_fid);
-          front_remaining -= 1;
-          back_remaining -= 1;
+          move_back_to_first_available(back_fid, result, back_remaining, front_remaining);
         }
       }
       front_it += 1;
