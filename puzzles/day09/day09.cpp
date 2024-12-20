fa84d927d24dd254c1dac49aa53fd46879993bdb
Bryan Covell <bpc6@vt.edu>
2024-12-20 11:34:33 -0700

    calculating files but need to fix checksum

--- puzzles/day09/day09.cpp
+++ puzzles/day09/day09.cpp
@@ -74,63 +74,51 @@ size_t part1(const std::string &filename) {
   return calc_checksum(compress_disc(readLines(PARENT_DIR "/" + filename)[0]));
 }
 
-void find_file_that_fits(size_t front_fid, size_t back_fid, int front_remaining,
-                         int back_remaining, auto back_it,
-                         std::vector<size_t> &result) {
-  while (back_fid > front_fid) {
-    if (back_remaining <= front_remaining) {
-      std::ranges::copy(std::vector<size_t>(back_remaining, back_fid),
-                        std::back_inserter(result));
-      return;
+struct file {
+  size_t id;
+  int size;
+  size_t loc;
+};
+
+struct space {
+  int size;
+  size_t loc;
+};
+
+std::pair<std::vector<file>, std::vector<space>>
+every_other(const std::string &intseq) {
+  std::vector<file> left;
+  std::vector<space> right;
+  size_t curr_loc{0};
+  for (auto [idx, c] : std::views::enumerate(intseq)) {
+    if (idx % 2 == 0) {
+      left.push_back(file(idx / 2, c - '0', curr_loc));
+    } else {
+      right.push_back(space(c - '0', curr_loc));
     }
-    advance_back(back_it, back_remaining, back_fid);
+    curr_loc += c - '0';
   }
+  return {left, right};
 }
 
 std::vector<size_t> compress_disc_whole_files(const std::string &discmap) {
-  auto back_fid = discmap.size() / 2;
-  auto back_it = discmap.rbegin();
-
-  size_t front_fid = 0;
-  bool front_is_file = true;
-  auto front_it = discmap.begin();
-
-  auto back_remaining = *back_it - '0';
-  auto front_remaining = *front_it - '0';
-  std::vector<size_t> result{};
-  while (back_fid > front_fid) {
-
-    if (front_is_file) {
-      std::ranges::copy(std::vector<size_t>(front_remaining, front_fid),
-                        std::back_inserter(result));
-      front_it += 1;
-      front_remaining = *front_it - '0';
-      front_is_file = false;
-    } else if (back_remaining < front_remaining) {
-      std::ranges::copy(std::vector<size_t>(back_remaining, back_fid),
-                        std::back_inserter(result));
-      front_remaining -= back_remaining;
-      advance_back(back_it, back_remaining, back_fid);
-    } else if (front_remaining < back_remaining) {
-      find_file_that_fits(front_fid, back_fid, front_remaining, back_remaining,
-                          back_it, result);
-
-      advance_front(front_it, front_remaining, front_fid);
-      advance_back(back_it, back_remaining, back_fid);
-      front_is_file = true;
-    }
-
-    else {
-      std::ranges::copy(std::vector<size_t>(back_remaining, back_fid),
-                        std::back_inserter(result));
-      advance_back(back_it, back_remaining, back_fid);
-      advance_front(front_it, front_remaining, front_fid);
-      front_is_file = true;
+  auto [files, spaces] = every_other(discmap);
+  std::ranges::reverse(files);
+  for (auto &file : files) {
+    for (auto &space : spaces) {
+      if (file.size <= space.size) {
+        file.loc = space.loc;
+        space.size -= file.size;
+        space.loc += file.size;
+        break;
+      }
     }
   }
 
-  if (front_is_file) {
-    std::ranges::copy(std::vector<size_t>(back_remaining, back_fid),
+  std::ranges::sort(files, [](file f1, file f2) { return f1.loc < f2.loc; });
+  std::vector<size_t> result;
+  for (const auto &file : files) {
+    std::ranges::copy(std::vector<size_t>(file.size, file.id),
                       std::back_inserter(result));
   }
   return result;
