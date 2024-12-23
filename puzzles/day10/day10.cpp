7abae6393192eb6e57935d0d18df21d1c6ffd484
Bryan Covell <bpc6@vt.edu>
2024-12-23 10:55:02 -0700

    only 10.2, gotta make pt 1 work again

--- puzzles/day10/day10.cpp
+++ puzzles/day10/day10.cpp
@@ -8,18 +8,15 @@ size_t count_trails_to_9(const Grid<int> &grid, Eigen::Vector2i curr) {
   std::stack<Eigen::Vector2i> stack;
   stack.push(curr);
 
-  std::vector<Eigen::Vector2i> seen;
   size_t count = 0;
   while (!stack.empty()) {
     curr = stack.top();
     stack.pop();
 
-    if (grid[curr] == 9 && std::ranges::find(seen, curr) == seen.end()) {
-      seen.push_back(curr);
+    if (grid[curr] == 9) {
       count += 1;
     } else {
       for (const auto &dir : grid.directions()) {
-        const auto next = (curr + dir).eval();
         if (grid.has_coord(curr + dir) && grid[curr + dir] == grid[curr] + 1) {
           stack.emplace(curr + dir);
         }
@@ -27,6 +24,14 @@ size_t count_trails_to_9(const Grid<int> &grid, Eigen::Vector2i curr) {
     }
   }
   return count;
+}
+
+size_t count_trail_ratings(const Grid<int> &grid) {
+  return std::ranges::fold_left(
+      grid.find_coords(0) | std::views::transform([&grid](const auto &coord) {
+        return count_trails_to_9(grid, coord);
+      }),
+      0, std::plus<>{});
 }
 
 size_t count_trail_scores(const Grid<int> &grid) {
@@ -41,4 +46,6 @@ size_t part1(const std::string &filename) {
   return count_trail_scores(Grid(readIntMatrix(PARENT_DIR "/" + filename)));
 }
 
-size_t part2(const std::string &filename) { return 0; }
+size_t part2(const std::string &filename) {
+  return count_trail_ratings(Grid(readIntMatrix(PARENT_DIR "/" + filename)));
+}
--- puzzles/day10/day10_test.cpp
+++ puzzles/day10/day10_test.cpp
@@ -2,4 +2,4 @@
 #include <gtest/gtest.h>
 
 TEST(Day10Test, Part1) { EXPECT_EQ(part1("test.txt"), 36); }
-TEST(Day10Test, Part2) { EXPECT_EQ(part2("test.txt"), 0); }
+TEST(Day10Test, Part2) { EXPECT_EQ(part2("test.txt"), 81); }
