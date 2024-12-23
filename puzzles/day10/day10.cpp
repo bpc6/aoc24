718271ceb01009a1b7eb9136ccd2f907c8fd4bb6
Bryan Covell <bpc6@vt.edu>
2024-12-23 10:59:02 -0700

    both parts passing but lots of duplicate

--- puzzles/day10/day10.cpp
+++ puzzles/day10/day10.cpp
@@ -26,6 +26,30 @@ size_t count_trails_to_9(const Grid<int> &grid, Eigen::Vector2i curr) {
   return count;
 }
 
+size_t count_unique_trails_to_9(const Grid<int> &grid, Eigen::Vector2i curr) {
+  std::stack<Eigen::Vector2i> stack;
+  stack.push(curr);
+  std::vector<Eigen::Vector2i> seen;
+
+  size_t count = 0;
+  while (!stack.empty()) {
+    curr = stack.top();
+    stack.pop();
+
+    if (grid[curr] == 9 && std::ranges::find(seen, curr) == seen.end()) {
+      seen.push_back(curr);
+      count += 1;
+    } else {
+      for (const auto &dir : grid.directions()) {
+        if (grid.has_coord(curr + dir) && grid[curr + dir] == grid[curr] + 1) {
+          stack.emplace(curr + dir);
+        }
+      }
+    }
+  }
+  return count;
+}
+
 size_t count_trail_ratings(const Grid<int> &grid) {
   return std::ranges::fold_left(
       grid.find_coords(0) | std::views::transform([&grid](const auto &coord) {
@@ -37,7 +61,7 @@ size_t count_trail_ratings(const Grid<int> &grid) {
 size_t count_trail_scores(const Grid<int> &grid) {
   return std::ranges::fold_left(
       grid.find_coords(0) | std::views::transform([&grid](const auto &coord) {
-        return count_trails_to_9(grid, coord);
+        return count_unique_trails_to_9(grid, coord);
       }),
       0, std::plus<>{});
 }
