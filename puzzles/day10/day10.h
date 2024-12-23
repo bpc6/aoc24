f0de5f865de9c2bd818e37ecf8867e3736eb4d12
Bryan Covell <bpc6@vt.edu>
2024-12-22 22:11:23 -0700

    day10 template

---
+++ puzzles/day10/day10.h
@@ -0,0 +1,10 @@
+#ifndef AOC24_DAY10_H
+#define AOC24_DAY10_H
+
+#include <string>
+#include <vector>
+
+long part1(const std::string &filename);
+long part2(const std::string &filename);
+
+#endif // AOC24_DAY10_H
