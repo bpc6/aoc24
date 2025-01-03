1db2d23d20c9466a6e2b760d44d9741ccf6392e8
Bryan Covell <bpc6@vt.edu>
2025-01-02 20:59:11 -0700

    use vector instead of tree

--- puzzles/day11/day11.cpp
+++ puzzles/day11/day11.cpp
@@ -1,19 +1,65 @@
 #include "day11.h"
-#include "binary_tree.h"
 #include "parsing.h"
 #include <algorithm>
+#include <cmath>
+#include <map>
 #include <ranges>
 
-size_t part1(const std::string &filename) {
-  BinaryTree tree{};
-  for (const auto &n : ints(readFile(PARENT_DIR "/" + filename))) {
-    tree.insert(n);
-  }
-
-  for (int i = 0; i < 25; ++i) {
-    tree.update();
-  }
-  return tree.count_leaves();
+unsigned long count_digits(unsigned long n) {
+  if (n == 0)
+    return 1;
+  return static_cast<unsigned long>(std::log10(n)) + 1;
 }
 
-size_t part2(const std::string &filename) { return 0; }
+std::pair<unsigned long, unsigned long> split_digits(unsigned long n) {
+  size_t half_digits = (static_cast<size_t>(std::log10(n)) + 1) / 2;
+  auto divisor = static_cast<unsigned long>(std::pow(10, half_digits));
+  return {n / divisor, n % divisor};
+}
+
+void update(std::vector<size_t> &curr,
+            std::map<size_t, std::vector<size_t>> &map) {
+  std::vector<size_t> next;
+  for (auto item : curr) {
+    if (item == 0) {
+      next.push_back(1);
+    } else if (count_digits(item) % 2 == 0) {
+      const auto [left, right] = split_digits(item);
+      next.push_back(left);
+      next.push_back(right);
+    } else {
+      next.push_back(item * 2024);
+    }
+  }
+  curr = std::move(next);
+}
+
+size_t size_after_updates(std::vector<size_t> curr, int count) {
+  std::map<size_t, std::vector<size_t>> map;
+  for (const auto item : curr) {
+    map[item] = {1};
+  }
+
+  for (int i = 0; i < count; ++i) {
+    update(curr, map);
+  }
+  return curr.size();
+}
+
+size_t part1(const std::string &filename) {
+  std::vector<size_t> curr;
+  for (const auto &n : ints(readFile(PARENT_DIR "/" + filename))) {
+    curr.push_back(n);
+  }
+
+  return size_after_updates(curr, 25);
+}
+
+size_t part2(const std::string &filename) {
+  std::vector<size_t> curr;
+  for (const auto &n : ints(readFile(PARENT_DIR "/" + filename))) {
+    curr.push_back(n);
+  }
+
+  return size_after_updates(curr, 75);
+}
