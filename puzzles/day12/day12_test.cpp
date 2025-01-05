91e29b36dffe684be27e91cef6a24b8f19aae827
Bryan Covell <bpc6@vt.edu>
2025-01-05 15:27:55 -0700

    tests

--- puzzles/day12/day12_test.cpp
+++ puzzles/day12/day12_test.cpp
@@ -1,5 +1,7 @@
 #include "day12.h"
 #include <gtest/gtest.h>
 
-TEST(Day12Test, Part1) { EXPECT_EQ(part1("test.txt"), 0); }
+TEST(Day12Test, Part1Test1) { EXPECT_EQ(part1("test1.txt"), 140); }
+TEST(Day12Test, Part1Test2) { EXPECT_EQ(part1("test2.txt"), 772); }
+TEST(Day12Test, Part1) { EXPECT_EQ(part1("test.txt"), 1930); }
 TEST(Day12Test, Part2) { EXPECT_EQ(part2("test.txt"), 0); }
