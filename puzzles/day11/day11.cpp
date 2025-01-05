f0755b932f2005aa9e234dc5f42f018d8ba91b24
Bryan Covell <bpc6@vt.edu>
2025-01-05 14:32:26 -0700

    complete day11

--- puzzles/day11/day11.cpp
+++ puzzles/day11/day11.cpp
@@ -18,21 +18,30 @@ std::pair<unsigned long, unsigned long> split_digits(unsigned long n) {
 }
 
 size_t size_after_updates(size_t curr, int count,
-                          std::map<size_t, std::vector<size_t>> &cache) {
-  if (count == 0)
-    return 1;
-  if (curr == 0)
-    return size_after_updates(1, count - 1, cache);
-  if (count_digits(curr) % 2 == 0) {
-    const auto [left, right] = split_digits(curr);
-    return size_after_updates(left, count - 1, cache) +
-           size_after_updates(right, count - 1, cache);
+                          std::map<size_t, std::map<size_t, size_t>> &cache) {
+  if (cache.contains(curr) && cache[curr].contains(count)) {
+    return cache[curr][count];
   }
-  return size_after_updates(curr * 2024, count - 1, cache);
+
+  size_t result;
+
+  if (count == 0)
+    result = 1;
+  else if (curr == 0)
+    result = size_after_updates(1, count - 1, cache);
+  else if (count_digits(curr) % 2 == 0) {
+    const auto [left, right] = split_digits(curr);
+    result = size_after_updates(left, count - 1, cache) +
+             size_after_updates(right, count - 1, cache);
+  } else
+    result = size_after_updates(curr * 2024, count - 1, cache);
+
+  cache[curr][count] = result;
+  return result;
 }
 
 size_t part1(const std::string &filename) {
-  std::map<size_t, std::vector<size_t>> cache;
+  std::map<size_t, std::map<size_t, size_t>> cache;
 
   return std::ranges::fold_left(ints(readFile(PARENT_DIR "/" + filename)) |
                                     std::views::transform([&cache](int a) {
@@ -42,7 +51,7 @@ size_t part1(const std::string &filename) {
 }
 
 size_t part2(const std::string &filename) {
-  std::map<size_t, std::vector<size_t>> cache;
+  std::map<size_t, std::map<size_t, size_t>> cache;
 
   return std::ranges::fold_left(ints(readFile(PARENT_DIR "/" + filename)) |
                                     std::views::transform([&cache](int a) {
