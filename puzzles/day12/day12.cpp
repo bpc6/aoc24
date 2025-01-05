a44567f9d66b3d3bd2dbd3b250c2d0f9b10017c1
Bryan Covell <bpc6@vt.edu>
2025-01-05 16:05:56 -0700

    passing small tests but not main p1 test

--- puzzles/day12/day12.cpp
+++ puzzles/day12/day12.cpp
@@ -1,7 +1,56 @@
 #include "day12.h"
+#include "grid.h"
 #include "parsing.h"
 #include <numeric>
+#include <stack>
 
-long part1(const std::string &filename) { return 0; }
+size_t perimeter(const Grid<char> &grid, char c) {
+  size_t perim = 0;
+  for (const auto &pos : grid.find_coords(c)) {
+    for (const auto &dir : grid.directions()) {
+      if (!grid.has_coord(pos + dir) || grid[pos + dir] != c)
+        perim += 1;
+    }
+  }
+  return perim;
+}
 
-long part2(const std::string &filename) { return 0; }
+size_t count_groups(const Grid<char> &grid, char c) {
+  size_t groups = 0;
+  std::vector<Eigen::Vector2i> seen;
+  for (const auto &pos : grid.find_coords(c)) {
+    if (std::ranges::find(seen, pos) == seen.end()) {
+
+      std::stack<Eigen::Vector2i> stack;
+      stack.push(pos);
+      while (!stack.empty()) {
+        auto curr = stack.top();
+        stack.pop();
+        seen.push_back(curr);
+
+        for (const auto &dir : grid.directions()) {
+          if (grid.has_coord(curr + dir) && grid[curr + dir] == c &&
+              std::ranges::find(seen, curr + dir) == seen.end()) {
+            stack.emplace(curr + dir);
+          }
+        }
+      }
+      groups += 1;
+    }
+  }
+  return groups;
+}
+
+size_t part1(const std::string &filename) {
+  Grid<char> grid{readCharMatrix(PARENT_DIR "/" + filename)};
+
+  size_t total = 0;
+  for (auto c : grid.find_unique()) {
+    total +=
+        perimeter(grid, c) * grid.find_coords(c).size() / count_groups(grid, c);
+  }
+
+  return total;
+}
+
+size_t part2(const std::string &filename) { return 0; }
