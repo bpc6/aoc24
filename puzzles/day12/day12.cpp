5d0f13f46f90afd860f969ecbdef1fa838c36919
Bryan Covell <bpc6@vt.edu>
2025-01-05 15:17:26 -0700

    day12 template

---
+++ puzzles/day12/day12.cpp
@@ -0,0 +1,7 @@
+#include "day12.h"
+#include "parsing.h"
+#include <numeric>
+
+long part1(const std::string &filename) { return 0; }
+
+long part2(const std::string &filename) { return 0; }
---
+++ puzzles/day12/day12.h
@@ -0,0 +1,10 @@
+#ifndef AOC24_DAY12_H
+#define AOC24_DAY12_H
+
+#include <string>
+#include <vector>
+
+long part1(const std::string &filename);
+long part2(const std::string &filename);
+
+#endif // AOC24_DAY12_H
