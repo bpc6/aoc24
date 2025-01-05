1743fd63c1f87d08df91496e3ff13ffd5eb91612
Bryan Covell <bpc6@vt.edu>
2025-01-05 14:37:39 -0700

    remove day12 stuff

--- puzzles/day12/day12.cpp
+++
@@ -1,7 +0,0 @@
-#include "day12.h"
-#include "parsing.h"
-#include <numeric>
-
-long part1(const std::string &filename) { return 0; }
-
-long part2(const std::string &filename) { return 0; }
--- puzzles/day12/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "day12.h"
-#include <iostream>
-
-int main() {
-  std::cout << part1("input.txt") << std::endl;
-  std::cout << part2("input.txt") << std::endl;
-  return EXIT_SUCCESS;
-}
