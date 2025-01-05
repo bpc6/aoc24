8c265ee7cb2a187e2404c534602a9873dc6e953c
Bryan Covell <bpc6@vt.edu>
2025-01-05 10:16:55 -0700

    add cache

--- puzzles/day11/day11.cpp
+++ puzzles/day11/day11.cpp
@@ -17,31 +17,36 @@ std::pair<unsigned long, unsigned long> split_digits(unsigned long n) {
   return {n / divisor, n % divisor};
 }
 
-size_t size_after_updates(size_t curr, int count) {
+size_t size_after_updates(size_t curr, int count,
+                          std::map<size_t, std::vector<size_t>> &cache) {
   if (count == 0)
     return 1;
   if (curr == 0)
-    return size_after_updates(1, count - 1);
+    return size_after_updates(1, count - 1, cache);
   if (count_digits(curr) % 2 == 0) {
     const auto [left, right] = split_digits(curr);
-    return size_after_updates(left, count - 1) +
-           size_after_updates(right, count - 1);
+    return size_after_updates(left, count - 1, cache) +
+           size_after_updates(right, count - 1, cache);
   }
-  return size_after_updates(curr * 2024, count - 1);
+  return size_after_updates(curr * 2024, count - 1, cache);
 }
 
 size_t part1(const std::string &filename) {
+  std::map<size_t, std::vector<size_t>> cache;
+
   return std::ranges::fold_left(ints(readFile(PARENT_DIR "/" + filename)) |
-                                    std::views::transform([](int a) {
-                                      return size_after_updates(a, 25);
+                                    std::views::transform([&cache](int a) {
+                                      return size_after_updates(a, 25, cache);
                                     }),
                                 0, std::plus<>{});
 }
 
 size_t part2(const std::string &filename) {
+  std::map<size_t, std::vector<size_t>> cache;
+
   return std::ranges::fold_left(ints(readFile(PARENT_DIR "/" + filename)) |
-                                    std::views::transform([](int a) {
-                                      return size_after_updates(a, 75);
+                                    std::views::transform([&cache](int a) {
+                                      return size_after_updates(a, 75, cache);
                                     }),
                                 0, std::plus<>{});
 }
