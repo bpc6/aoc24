fe72a596302ad942eef0f8bc4d1177e7b630a181
Bryan Covell <bpc6@vt.edu>
2025-01-16 20:46:56 -0700

    part 2 works but oof its ugly

--- puzzles/day12/day12.cpp
+++ puzzles/day12/day12.cpp
@@ -58,6 +58,7 @@ size_t part1(const std::string &filename) {
 size_t discounted(const Grid<char> &grid, char c) {
   size_t total = 0;
   Grid<int> seen{grid.height(), grid.width(), 0};
+  Grid<int> processed{grid.height(), grid.width(), 0};
   for (const auto &pos : grid.find_coords(c)) {
     if (!seen[pos]) {
       size_t perim = 0;
@@ -68,15 +69,37 @@ size_t discounted(const Grid<char> &grid, char c) {
       while (!stack.empty()) {
         auto curr = stack.top();
         seen[curr] = 1;
+        processed[curr] = 1;
 
         stack.pop();
         area += 1;
-        perim += perimeter_coords(grid, curr).size();
+
+        auto ps = perimeter_coords(grid, curr);
+        perim += ps.size();
+
+        std::vector<Eigen::Vector2i> adj_seen;
         for (const auto &dir : grid.directions()) {
-          if (grid.has_coord(curr + dir) && grid[curr + dir] == c &&
-              !seen[curr + dir]) {
-            stack.emplace(curr + dir);
-            seen[curr + dir] = 1;
+          const auto adj = curr + dir;
+          if (grid.has_coord(adj) && processed[adj]) {
+            adj_seen.emplace_back(adj);
+          }
+        }
+
+        for (const auto &p : ps) {
+          const auto dir_to_p = (p - curr).eval();
+          for (const auto adj : adj_seen) {
+            const auto adj_ps = perimeter_coords(grid, adj);
+            if (std::ranges::find(adj_ps, adj + dir_to_p) != adj_ps.end()) {
+              perim -= 1;
+            }
+          }
+        }
+
+        for (const auto &dir : grid.directions()) {
+          const auto next = curr + dir;
+          if (grid.has_coord(next) && grid[next] == c && !seen[next]) {
+            stack.emplace(next);
+            seen[next] = 1;
           }
         }
       }
