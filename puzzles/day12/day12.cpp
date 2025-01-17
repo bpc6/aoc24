07c8168edc71e832454d75b237f2c00736b229b5
Bryan Covell <bpc6@vt.edu>
2025-01-16 19:53:54 -0700

    copied pt1 to pt2

--- puzzles/day12/day12.cpp
+++ puzzles/day12/day12.cpp
@@ -1,7 +1,6 @@
 #include "day12.h"
 #include "grid.h"
 #include "parsing.h"
-#include <numeric>
 #include <stack>
 
 std::vector<Eigen::Vector2i> perimeter_coords(const Grid<char> &grid,
@@ -56,4 +55,44 @@ size_t part1(const std::string &filename) {
   return total;
 }
 
-size_t part2(const std::string &filename) { return 0; }
+size_t discounted(const Grid<char> &grid, char c) {
+  size_t total = 0;
+  Grid<int> seen{grid.height(), grid.width(), 0};
+  for (const auto &pos : grid.find_coords(c)) {
+    if (!seen[pos]) {
+      size_t perim = 0;
+      size_t area = 0;
+
+      std::stack<Eigen::Vector2i> stack;
+      stack.push(pos);
+      while (!stack.empty()) {
+        auto curr = stack.top();
+        seen[curr] = 1;
+
+        stack.pop();
+        area += 1;
+        perim += perimeter_coords(grid, curr).size();
+        for (const auto &dir : grid.directions()) {
+          if (grid.has_coord(curr + dir) && grid[curr + dir] == c &&
+              !seen[curr + dir]) {
+            stack.emplace(curr + dir);
+            seen[curr + dir] = 1;
+          }
+        }
+      }
+      total += perim * area;
+    }
+  }
+  return total;
+}
+
+size_t part2(const std::string &filename) {
+  Grid<char> grid{readCharMatrix(PARENT_DIR "/" + filename)};
+
+  size_t total = 0;
+  for (auto c : grid.find_unique()) {
+    total += discounted(grid, c);
+  }
+
+  return total;
+}
