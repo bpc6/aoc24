8b482c69ae20cac0ed2b0b03a7ebf3c867cd9024
Bryan Covell <bpc6@vt.edu>
2025-02-28 07:48:47 -0700

    refactoring and add pt 2

--- puzzles/day13/day13.cpp
+++ puzzles/day13/day13.cpp
@@ -3,36 +3,47 @@
 #include <cctype>
 #include <ranges>
 
+using Vec = Eigen::Vector2i;
+
 int numeric_only(const std::string &s) {
   auto nums = s | std::views::filter([](char c) { return std::isdigit(c); });
   return std::stoi(std::string{nums.begin(), nums.end()});
 }
 
-Eigen::Vector2i parse_line(std::string line) {
+Vec parse_line(std::string line) {
   return {numeric_only(split(line, ',')[0]), numeric_only(split(line, ',')[1])};
+}
+
+std::tuple<Vec, Vec, Vec> parse_input(std::string paragraph) {
+  const auto lines = split(paragraph, '\n');
+  return {parse_line(lines[0]), parse_line(lines[1]), parse_line(lines[2])};
 }
 
 size_t part1(const std::string &filename) {
   size_t total = 0;
   auto txt = readFile(PARENT_DIR "/" + filename);
   for (const auto grp : split(txt, "\n\n")) {
-    const auto lines = split(grp, '\n');
-    auto a = parse_line(lines[0]);
-    auto b = parse_line(lines[1]);
-    auto p = parse_line(lines[2]);
+    auto [a, b, p] = parse_input(grp);
     total += cost_of_prize(a, b, p);
   }
   return total;
 }
 
-size_t part2(const std::string &filename) { return 0; }
+size_t part2(const std::string &filename) {
+  size_t total = 0;
+  auto txt = readFile(PARENT_DIR "/" + filename);
+  for (const auto grp : split(txt, "\n\n")) {
+    auto [a, b, p] = parse_input(grp);
+    total += cost_of_prize(a, b, p + Vec{0, 0});
+  }
+  return total;
+}
 
-size_t cost_of_prize(const Eigen::Vector2i &dir_a, const Eigen::Vector2i &dir_b,
-                     const Eigen::Vector2i &prize) {
-  Eigen::Vector2i curr = {0, 0};
+size_t cost_of_prize(const Vec &dir_a, const Vec &dir_b, const Vec &prize) {
+  Vec curr = {0, 0};
   int cost = 0;
   for (int a_depth : std::views::iota(0, 100)) {
-    Eigen::Vector2i tmp = curr;
+    Vec tmp = curr;
     int tmp_cost = cost;
     for (int b_depth : std::views::iota(0, 100)) {
       if (tmp == prize)
