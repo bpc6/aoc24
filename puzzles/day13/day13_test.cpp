614fa33a433864e17eb79cde8746098c7fc76429
Bryan Covell <bpc6@vt.edu>
2025-02-28 16:26:38 -0700

    split out big number tests that can't be found

--- puzzles/day13/day13_test.cpp
+++ puzzles/day13/day13_test.cpp
@@ -14,13 +14,16 @@ TEST(Day13Test, CostOfPrize0) {
   EXPECT_EQ(cost_of_prize({17, 86}, {84, 37}, {7870, 6450}), 200);
 }
 
-TEST(Day13Test, CostOfPrize1) {
-  EXPECT_EQ(cost_of_prize({94, 34}, {22, 67}, {10000000008400, 10000000005400}),
-            0);
+TEST(Day13Test, CostOfBigPrizePossible) {
   EXPECT_NE(cost_of_prize({26, 66}, {67, 21}, {10000000012748, 10000000012176}),
             0);
-  EXPECT_EQ(cost_of_prize({17, 86}, {84, 37}, {10000000007870, 10000000006450}),
-            0);
   EXPECT_NE(cost_of_prize({69, 23}, {27, 71}, {10000000018641, 10000000010279}),
+            0);
+}
+
+TEST(Day13Test, CostOfBigPrizeImpossible) {
+  EXPECT_EQ(cost_of_prize({94, 34}, {22, 67}, {10000000008400, 10000000005400}),
+            0);
+  EXPECT_EQ(cost_of_prize({17, 86}, {84, 37}, {10000000007870, 10000000006450}),
             0);
 }
