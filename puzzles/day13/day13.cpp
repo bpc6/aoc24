4d0465edb048a11d003e039684dea52efec4c7b6
Bryan Covell <bpc6@vt.edu>
2025-02-27 21:41:43 -0700

    algo fcn passes test

--- puzzles/day13/day13.cpp
+++ puzzles/day13/day13.cpp
@@ -1,5 +1,6 @@
 #include "day13.h"
 #include "parsing.h"
+#include <ranges>
 
 size_t part1(const std::string &filename) { return 0; }
 
@@ -7,5 +8,21 @@ size_t part2(const std::string &filename) { return 0; }
 
 size_t cost_of_prize(const Eigen::Vector2i &dir_a, const Eigen::Vector2i &dir_b,
                      const Eigen::Vector2i &prize) {
+  Eigen::Vector2i curr = {0, 0};
+  int cost = 0;
+  for (int a_depth : std::views::iota(0, 100)) {
+    Eigen::Vector2i tmp = curr;
+    int tmp_cost = cost;
+    for (int b_depth : std::views::iota(0, 100)) {
+      if (tmp == prize)
+        return tmp_cost;
+      if (tmp.x() > prize.x() or tmp.y() > prize.y())
+        break;
+      tmp += dir_b;
+      tmp_cost += 1;
+    }
+    curr += dir_a;
+    cost += 3;
+  }
   return 0;
 }
--- puzzles/day13/day13_test.cpp
+++ puzzles/day13/day13_test.cpp
@@ -6,4 +6,6 @@ TEST(Day13Test, Part2) { EXPECT_EQ(part2("test.txt"), 0); }
 
 TEST(Day13Test, CostOfPrize0) {
   EXPECT_EQ(cost_of_prize({94, 34}, {22, 67}, {8400, 5400}), 280);
+  EXPECT_EQ(cost_of_prize({26, 66}, {67, 21}, {12748, 12176}), 0);
+  EXPECT_EQ(cost_of_prize({17, 86}, {84, 37}, {7870, 6450}), 200);
 }
