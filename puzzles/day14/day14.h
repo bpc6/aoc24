96641d7b308191a130cf291e57e2e4e0e45810f6
Bryan Covell <bpc6@vt.edu>
2025-02-28 16:49:56 -0700

    day14 template

---
+++ puzzles/day14/day14.h
@@ -0,0 +1,10 @@
+#ifndef AOC24_DAY14_H
+#define AOC24_DAY14_H
+
+#include <Eigen/Dense>
+#include <string>
+
+size_t part1(const std::string &filename);
+size_t part2(const std::string &filename);
+
+#endif // AOC24_DAY14_H
