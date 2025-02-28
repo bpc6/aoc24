71f6705abfcaeb9aaaf0915a01cdaa5a2817fd03
Bryan Covell <bpc6@vt.edu>
2025-02-27 21:07:30 -0700

    Revert "started recursive strategy"
    This reverts commit c490930cf7411bb85ce0ca22b512e34cf2b81319.

--- puzzles/day13/day13.h
+++ puzzles/day13/day13.h
@@ -9,7 +9,6 @@ size_t part1(const std::string &filename);
 size_t part2(const std::string &filename);
 
 size_t cost_of_prize(const Eigen::Vector2i &dir_a, const Eigen::Vector2i &dir_b,
-                     const Eigen::Vector2i &prize, const int a_depth,
-                     const int b_depth);
+                     const Eigen::Vector2i &prize);
 
 #endif // AOC24_DAY13_H
