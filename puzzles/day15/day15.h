7416c2b4574f19eb5c2892de663bc4fbf75bc602
Bryan Covell <bpc6@vt.edu>
2025-03-22 10:05:16 -0600

    day15 template

---
+++ puzzles/day15/day15.h
@@ -0,0 +1,9 @@
+#ifndef AOC24_DAY15_H
+#define AOC24_DAY15_H
+
+#include <string>
+
+size_t part1(const std::string &filename);
+size_t part2(const std::string &filename);
+
+#endif // AOC24_DAY15_H
---
+++ puzzles/day15/day15_test.cpp
@@ -0,0 +1,6 @@
+#include "day15.h"
+#include <gtest/gtest.h>
+
+TEST(Day15Test, Part1) { EXPECT_EQ(part1("test.txt"), 0); }
+TEST(Day15Test, Part1Big) { EXPECT_EQ(part1("test_big.txt"), 0); }
+TEST(Day15Test, Part2) { EXPECT_EQ(part1("test.txt"), 0); }
